1f78b997dae427e98afe6e689d2632352367713c
Jiraporn Praneet <jirapornpraneet@Jiraporns-MacBook-Pro.local>
2017-07-25 10:26:26 +0700

    ดึงข้อมูลจากAPI ออกมาตามค่าสกุลเงินที่เราเลือกในตัวแปรชื่อ base

--- Currency/ViewController.swift
+++ Currency/ViewController.swift
@@ -14,24 +14,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var currencyFieldPicker: AAPickerView!
     @IBOutlet weak var currencyLabel: UILabel!
     @IBOutlet weak var ratesLabel: UILabel!
+    var selectDataCurrency = ""
     override func viewDidLoad() {
         super.viewDidLoad()
         configCurrencyFieldPickerPicker()
-        let url = String(format:"http://api.fixer.io/latest?base=%@", "USD")
-        Alamofire.request(url, method: .get).validate().responseJSON { response in
-            switch response.result {
-            case .success(let value):
-                let json = JSON(value)
-                print("JSON: \(json)")
-                print(response)
-                print("rates")
-                let show = json["rates"]["THB"]
-                print("JSON: \(show)")
-                self.currencyLabel.text = "\(show)"
-            case .failure(let error):
-                print(error)
-            }
-        }
     }
     func configCurrencyFieldPickerPicker() {
         currencyFieldPicker.pickerType = .StringPicker
@@ -42,8 +28,28 @@ class ViewController: UIViewController {
         currencyFieldPicker.toolbar.barTintColor = UIColor(red:0.90, green:0.37, blue:0.37, alpha:1.0)
         currencyFieldPicker.toolbar.tintColor = UIColor(red:0.90, green:0.37, blue:0.37, alpha:1.0)
         currencyFieldPicker.stringDidChange = { index in
-            print("selectedString1 ", dataCurrencies[index])
+             self.selectDataCurrency = dataCurrencies[index]
+            print("selectedString1 :  ", self.selectDataCurrency, dataCurrencies[index])
+            self.selectDataCurrenciesAPI(base:self.selectDataCurrency)
         }
+    }
+    func selectDataCurrenciesAPI(base:String) {
+        let url = String(format:"http://api.fixer.io/latest?base=%@", base)
+        Alamofire.request(url, method: .get).validate().responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                print("JSON: \(json)")
+                print(response)
+               print("rates")
+                let show = json["rates"]["THB"]
+                print("JSON: \(show)")
+                self.currencyLabel.text = "\(show)"
+            case .failure(let error):
+                print(error)
+            }
+        }
+
     }
 
     override func didReceiveMemoryWarning() {
